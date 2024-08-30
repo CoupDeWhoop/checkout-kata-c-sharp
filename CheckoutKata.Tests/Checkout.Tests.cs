1e640608f908e2fa74fce077a8495a9650afb073
CoupDeWhoop <greenland.david@gmail.com>
2024-08-30 21:12:41 +0100

    tests for single items

--- CheckoutKata.Tests/Checkout.Tests.cs
+++ CheckoutKata.Tests/Checkout.Tests.cs
@@ -4,17 +4,34 @@ namespace CheckoutKata.Tests;
 
 public class CheckoutTests
 {
+    private ICheckout _checkout;
+    
+    [SetUp]
+    public void SetUp()
+    {
+        _checkout = new Checkout();
+    }
 
     [Test]
     public void GetTotalPrice_ShouldReturnZero_WhenPassedNothing()
     {
-            // Arrange
-            ICheckout checkout = new Checkout();
+        int totalPrice = _checkout.GetTotalPrice();
 
-            // Act
-            int totalPrice = checkout.GetTotalPrice();
+        Assert.AreEqual(0, totalPrice);
+    }
 
-            // Assert
-            Assert.AreEqual(0, totalPrice);
+    [TestCase("A", 50)]
+    [TestCase("B", 30)]
+    [TestCase("C", 20)]
+    [TestCase("D", 15)]
+    public void GetTotalPrice_ShouldReturnCorrectly_WhenPassedSingleItem(string item, int expected)
+    {
+
+        _checkout.Scan(item);
+
+        int totalPrice = _checkout.GetTotalPrice();
+
+        // Assert
+        Assert.AreEqual(expected, totalPrice);
     }
 }
--- CheckoutKata/Checkout.cs
+++ CheckoutKata/Checkout.cs
@@ -2,12 +2,18 @@
 
 public class Checkout : ICheckout
 {
+    private string _basket;
+    
     public void Scan(string item)
     {
-
+        _basket = item;
     }
     public int GetTotalPrice()
     {
+        if (_basket == "A") return 50;
+        if (_basket == "B") return 30;
+        if (_basket == "C") return 20;
+        if (_basket == "D") return 15;
         return 0;
     }
 }
