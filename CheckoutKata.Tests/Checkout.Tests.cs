c16f66195a183280149c223307fb886725173804
CoupDeWhoop <greenland.david@gmail.com>
2024-08-31 12:36:30 +0100

    adds test for multiple items

--- CheckoutKata.Tests/Checkout.Tests.cs
+++ CheckoutKata.Tests/Checkout.Tests.cs
@@ -34,4 +34,21 @@ public class CheckoutTests
         // Assert
         Assert.AreEqual(expected, totalPrice);
     }
+
+    [TestCase("AA", 100)]
+    [TestCase("CCC", 60)]
+    [TestCase("DDDDD", 75)]
+    [TestCase("CACAC", 160)]
+    [TestCase("ABCD", 115)]
+
+    public void GetTotalPrice_ShouldReturnCorrectly_ForMultiples(string item, int expected)
+    {
+
+        _checkout.Scan(item);
+
+        int totalPrice = _checkout.GetTotalPrice();
+
+        // Assert
+        Assert.AreEqual(expected, totalPrice);
+    }
 }
--- CheckoutKata/Checkout.cs
+++ CheckoutKata/Checkout.cs
@@ -2,18 +2,42 @@
 
 public class Checkout : ICheckout
 {
-    private string _basket;
+    private List<char> _basket = new List<char>();
     
     public void Scan(string item)
     {
-        _basket = item;
+        var split = item.ToCharArray();
+        foreach(var sku in split)
+        {
+            _basket.Add(sku);
+        }
+        
     }
     public int GetTotalPrice()
     {
-        if (_basket == "A") return 50;
-        if (_basket == "B") return 30;
-        if (_basket == "C") return 20;
-        if (_basket == "D") return 15;
-        return 0;
+        int totalPrice = 0;
+        foreach(var sku in _basket)
+        {
+            totalPrice += GetPriceForSku(sku);
+        }
+
+        return totalPrice;
+    }
+
+    private int GetPriceForSku(char sku)
+    {
+        switch (sku)
+        {
+            case 'A':
+                return 50;
+            case 'B':
+                return 30;
+            case 'C':
+                return 20;
+            case 'D':
+                return 15;
+            default:
+                throw new ArgumentException($"Invalid SKU: {sku}");
+        }
     }
 }
